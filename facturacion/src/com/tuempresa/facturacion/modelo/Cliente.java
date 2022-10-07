041d5148600378f031a2fb23e0a0943e2d3e2a3c
sinaiHau <114089949+sinaiHau@users.noreply.github.com>
2022-10-07 15:47:55 -0500

    Lección cuatro
    Se agrego la lección 4

--- facturacion/src/com/tuempresa/facturacion/modelo/Cliente.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/Cliente.java
@@ -8,6 +8,7 @@ import lombok.*;
 
 @Entity
 @Getter @Setter
+@View(name = "Simple", members = "numero, nombre")
 
 
 public class Cliente {
--- facturacion/src/com/tuempresa/facturacion/modelo/Factura.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/Factura.java
@@ -15,6 +15,12 @@ import com.tuempresa.facturacion.calculadores.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+@View(members =
+        "anyo, numero, fecha;"+
+        "cliente;"+
+        "detalles;"+
+        "observaciones")
+
 public class Factura {
 	
 	
@@ -40,6 +46,7 @@ public class Factura {
 	LocalDate fecha;
 	
 	@ManyToOne(fetch =FetchType.LAZY, optional = false)
+	@ReferenceView("Simple")
 	Cliente cliente;
 	
 	@ElementCollection
@@ -49,6 +56,7 @@ public class Factura {
 	
 	@Stereotype("MEMO")
 	String observaciones;
+	
 	
 
 }
