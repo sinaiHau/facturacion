a1a65694d6be0d4bc594f4509a24524247a14578
sinaiHau <114089949+sinaiHau@users.noreply.github.com>
2022-10-19 19:52:27 -0500

    lógica

--- facturacion/src/com/tuempresa/facturacion/modelo/Detalle.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/Detalle.java
@@ -1,12 +1,6 @@
 package com.tuempresa.facturacion.modelo;
 
-import java.math.*;
-
 import javax.persistence.*;
-
-import org.openxava.annotations.*;
-
-import com.tuempresa.facturacion.calculadores.*;
 
 import lombok.*;
 
@@ -17,18 +11,5 @@ public class Detalle {
 	 
 	 @ManyToOne(fetch =FetchType.LAZY, optional = true)
 	 Producto producto;
-	 
-	 @Stereotype("DINERO")
-	 @Depends("precioPorUnidad, cantidad")
-	 public BigDecimal getImporte() {
-		 if(precioPorUnidad == null)return BigDecimal.ZERO;
-		 return new BigDecimal(cantidad).multiply(precioPorUnidad);
-	 }
-	 
-	@DefaultValueCalculator(value = CalculadorPrecioPorUnidad.class,
-			properties = @PropertyValue(name = "numeroProducto",from = "producto.numero"))
-	
-	 @Stereotype("DINERO")
-	 BigDecimal precioPorUnidad;
 
 }
--- facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
+++ facturacion/src/com/tuempresa/facturacion/modelo/DocumentoComercial.java
@@ -1,9 +1,11 @@
 package com.tuempresa.facturacion.modelo;
 
+import java.math.*;
 import java.time.*;
 import java.util.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
@@ -43,13 +45,30 @@ abstract public class DocumentoComercial extends Identificable {
 	Cliente cliente;
 	
 	@ElementCollection
-	@ListProperties("producto.numero, producto.descripcion,cantidad,precioPorUnidad, importe")
+	@ListProperties("producto.numero, producto.descripcion,cantidad, precioPorUnidad,"
+			+ "importe+["
+			+  "doucumntoComercial.porcentajeIVA,"
+			+  "doucumentoComercial.iva,"
+			+  "doucumentoComercial.importeTotal"
+			+ "]"
+			)
 	Collection<Detalle> detalles;
 	
 	
 	@Stereotype("MEMO")
 	String observaciones;
+	 
+	@Digits(integer=2, fraction=0)
+	BigDecimal porcentajeIVA;
 	
+	@ReadOnly
+	@Stereotype("DINERO")
+	@Calculation("sum(detalles.importe)*porcentajeIVA/100")
+	BigDecimal iva;
 	
+	@ReadOnly
+	@Stereotype("DINERO")
+	@Calculation("sum(detalle.importe)+iva")
+	BigDecimal importeTotal;
 
 }
